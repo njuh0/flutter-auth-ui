623f736e7b2f5c66996a5b0f273a913e1f1d1ced
Edgar <edgar4443@live.com>
2023-06-09 16:54:50 +0300

    Update supa_socials_auth.dart
    remove showSnackBar

--- lib/src/components/supa_socials_auth.dart
+++ lib/src/components/supa_socials_auth.dart
@@ -139,7 +139,7 @@ class _SupaSocialsAuthState extends State<SupaSocialsAuth> {
       final session = data.session;
       if (session != null && mounted) {
         widget.onSuccess.call(session);
-        context.showSnackBar('Successfully signed in !');
+//         context.showSnackBar('Successfully signed in !');
       }
     });
   }
